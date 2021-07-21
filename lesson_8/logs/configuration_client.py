012652417b89fb8bcba0d866cd627b17807daaa6
Ramilia <63601627+ra-110110@users.noreply.github.com>
2021-07-22 02:48:00 +0300

    Add files via upload

---
+++ lesson_8/logs/configuration_client.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import logging
+
+from utils.utils import load_configs
+
+sys.path.append('../')
+
+CONFIGS = load_configs()
+
+CLIENT_FORMATTER = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(PATH, 'client.log')
+
+STREAM_HANDLER = logging.StreamHandler(sys.stderr)
+STREAM_HANDLER.setFormatter(CLIENT_FORMATTER)
+STREAM_HANDLER.setLevel(logging.ERROR)
+LOG_FILE = logging.FileHandler(PATH, encoding='utf8')
+LOG_FILE.setFormatter(CLIENT_FORMATTER)
+
+LOGGER = logging.getLogger('client')
+LOGGER.addHandler(STREAM_HANDLER)
+LOGGER.addHandler(LOG_FILE)
+LOGGER.setLevel(CONFIGS.get('LOGGING_LEVEL', logging.DEBUG))
+
+if __name__ == '__main__':
+    LOGGER.critical('Критическая ошибка')
+    LOGGER.error('Ошибка')
+    LOGGER.debug('Отладочная информация')
+    LOGGER.info('Информационное сообщение')
